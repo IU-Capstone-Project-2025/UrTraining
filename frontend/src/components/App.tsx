8de0c42cf5361dace2ac816b9c8aa436833f2792
Gray <makar.dyachenko@proton.me>
2025-06-11 15:33:09 +0300

    Navbar start

---
+++ frontend/src/components/App.tsx
@@ -0,0 +1,16 @@
+// import React from 'react';
+import '../css/App.css';
+import './Navbar';
+import Header from './Navbar';
+
+const App = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default App;
+
+
