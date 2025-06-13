10d07fcfbcfcd882895710e3bab0cb4ce24a0f89
IlonaDzuirava <il.dziurava@innopolis.university>
2025-06-13 16:53:23 +0300

    login page part 2

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -4,7 +4,7 @@ import Login from "../components/Login";
 const LoginPage = () => {
   return (
     <div>
-      <div className="navbar__logo">
+      <div className="login__navbar__logo">
         <Link to="/">
           <h1>URTRAINING</h1>
         </Link>
