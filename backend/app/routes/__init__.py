a92d8de0562e6694b86c0afc78ac01f759334dad
Ildar Rakiev <157794777+IldarRakiev@users.noreply.github.com>
2025-06-21 15:38:29 +0300

    Revert "Salavat/backend"

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -1,1 +1,8 @@
-# This file is now empty as all routes except auth have been removed
+from fastapi import APIRouter
+from .base import router as base_router
+from .programs import router as programs_router
+
+router = APIRouter()
+
+router.include_router(base_router)
+router.include_router(programs_router)
