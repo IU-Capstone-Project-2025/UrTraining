e9402ac2e115b9bf68866d97dc5e368429709073
IldarRakiev <aldir0707@mail.ru>
2025-06-26 21:17:58 +0500

    Catalogue of trainings is connected to the trainings data API endpoint now

--- frontend/src/api/apiRequests.ts
+++ frontend/src/api/apiRequests.ts
@@ -42,6 +42,23 @@ export async function surveyDataRequest(token: String): Promise<SurveyProp> {
     }
 }
 
+export async function trainingsDataRequest(token: String): Promise<any> {
+    try {
+        const resp = await axios.get<String>(`${endpoint}/trainings`, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
+        console.log("бро красава")
+        return resp.data;
+    } catch (error) {
+        if (axios.isAxiosError(error) && error.response) {
+            throw error;
+        }
+        throw error;
+    }
+}
+
 export async function signUpRequest(
     credentials: CredentialsData
 ): Promise<SignUpSuccess> {
